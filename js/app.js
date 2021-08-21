7fad7c458ef5b62daa2383b3ca1ad24d22983e29
ebfaler <74653573+ebfaler@users.noreply.github.com>
2021-08-21 14:05:03 +0200

    Updated app.js and index.html
    Created search functionality using js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,30 @@
-baguetteBox.run('.gallery');
-const search = new Filter('search', 'data-caption');
+// window.addEventListener('load', function() {
+    
+//     const search = new Filter('search', 'data-caption');
+// });
 
+
+//Work in progress
+
+baguetteBox.run('.gallery');
+
+const search = document.querySelector('#search');
+const boxTexts = document.querySelectorAll('[data-caption]');
+
+const handleSearch = event =>  {
+
+const searchTerm = event.target.value.toLowerCase();
+
+       boxTexts.forEach(boxText => {
+       const text = boxText.getAttribute('data-caption').toLowerCase();
+        
+       if(text.includes(searchTerm)) {
+            boxText.style.display = "block";
+          } else {
+            boxText.style.display = "none";  
+          }
+      });
+
+}
+
+search.addEventListener('keyup', handleSearch);   
