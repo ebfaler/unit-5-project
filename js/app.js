feb17d7d7526dedd8a2e260ac74111111b00f9cf
ebfaler <74653573+ebfaler@users.noreply.github.com>
2021-08-23 12:30:01 +0200

    updated display

--- js/app.js
+++ js/app.js
@@ -15,13 +15,13 @@ const handleSearch = (event) =>  {
 
   const searchTerm = event.target.value.toLowerCase();
 
-  boxTexts.forEach(boxText => {
-    const text = boxText.getAttribute('data-caption').toLowerCase();
+  boxTexts.forEach(boxTexts => {
+    const text = boxTexts.getAttribute('data-caption').toLowerCase();
           
     if(text.includes(searchTerm)) {
-      boxText.style.display = "block";
+      boxTexts.parentElement.style.display = "block";
     } else {
-      boxText.style.display = "none";  
+      boxTexts.parentElement.style.display = "none";  
     }
   });
 
