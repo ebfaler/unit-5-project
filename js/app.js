2fa06b850ef98005c613c5b392fea5f75e922c9a
ebfaler <74653573+ebfaler@users.noreply.github.com>
2021-08-23 12:07:25 +0200

    reformatted

--- js/app.js
+++ js/app.js
@@ -11,20 +11,20 @@ baguetteBox.run('.gallery');
 const search = document.querySelector('#search');
 const boxTexts = document.querySelectorAll('[data-caption]');
 
-const handleSearch = event =>  {
+const handleSearch = (event) =>  {
 
-const searchTerm = event.target.value.toLowerCase();
+  const searchTerm = event.target.value.toLowerCase();
 
-       boxTexts.forEach(boxText => {
-       const text = boxText.getAttribute('data-caption').toLowerCase();
-        
-       if(text.includes(searchTerm)) {
-            boxText.style.display = "block";
-          } else {
-            boxText.style.display = "none";  
-          }
-      });
+  boxTexts.forEach(boxText => {
+    const text = boxText.getAttribute('data-caption').toLowerCase();
+          
+    if(text.includes(searchTerm)) {
+      boxText.style.display = "block";
+    } else {
+      boxText.style.display = "none";  
+    }
+  });
 
-}
+};
 
 search.addEventListener('keyup', handleSearch);   
